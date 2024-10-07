b9e38e2dd56e1a1fcd353cab22489b85da71cb80
Spring-Mirage <spring-mirage>
2024-10-06 23:06:19 -0500

    Feat: Param and Query

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 //localhost:3000/users
 @Controller('users')
 export class UsersController {
-
 	@Get()
-	getUsers() {
+	getUsers(@Query() query: any) {
         const usersService = new UsersService();
+        if (query.gender) {
+            return usersService.getAllUsers().filter(u => u.gender === query.gender)
+        }
         return usersService.getAllUsers();
+    }
+
+    //when you use '?' ':id/:name/:gender?' is optional parammeter
+    //if you want to get all params don't put anything in the Params decorator and put param: any
+    @Get(':id')
+    getUserById(@Param('id') id: any) {
+        const usersService = new UsersService();
+        return usersService.getUserById(+id);   
     }
 
 	@Post()
